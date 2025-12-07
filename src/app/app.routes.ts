8832141d006d53a08a830c2a9ba50700bb1be77c
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-12-07 12:29:52 -0300

    configura module federation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'bytebank-home-angular',
+    loadComponent: () => {
+      return loadRemoteModule('bytebank-home-angular', './Component').then((m) => m.AppComponent)
+    }
+
+  },
+  {
+    path: 'bytebank-dashboard-angular',
+    loadComponent: () => {
+      return loadRemoteModule('bytebank-dashboard-angular', './Component').then((m) => m.AppComponent)
+    },
+  }
+];
