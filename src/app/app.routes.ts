55b7d91c90066ff1866f8f779a4325bae3d6db56
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-12-09 22:48:24 -0300

    cria pagina para erro 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,23 @@
 import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
-    path: 'bytebank-home-angular',
+    path: 'home',
     loadComponent: () => {
       return loadRemoteModule('bytebank-home-angular', './Component').then((m) => m.AppComponent)
     }
 
   },
   {
-    path: 'bytebank-dashboard-angular',
+    path: 'dashboard',
     loadComponent: () => {
       return loadRemoteModule('bytebank-dashboard-angular', './Component').then((m) => m.AppComponent)
     },
-  }
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
