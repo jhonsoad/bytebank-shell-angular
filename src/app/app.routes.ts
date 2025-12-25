04ce6fe7f3bc1f95495710cf58e859d4f6ca585e
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-12-25 11:38:35 -0300

    ajusta nome das variaveis css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,24 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     loadComponent: () => {
       return loadRemoteModule('bytebank-home-angular', './Component').then((m) => m.AppComponent)
     }
-
   },
+  // {
+  //   path: 'bytebank', // O prefixo na URL da Shell
+  //   loadChildren: () =>
+  //     loadRemoteModule('bytebank-home-angular', './routes')
+  //       .then((m) => m.routes)
+  // },
   {
     path: 'dashboard',
     loadComponent: () => {
